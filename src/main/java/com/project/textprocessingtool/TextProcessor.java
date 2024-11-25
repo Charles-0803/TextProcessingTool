fed1672e1251c87851e63a15629c8cec2e1ca0d5
Charles-0803 <charles.akasoma@amalitech.com>
2024-11-25 16:10:26 +0000

    Update TextProcessor.java

--- src/main/java/com/project/textprocessingtool/TextProcessor.java
+++ src/main/java/com/project/textprocessingtool/TextProcessor.java
@@ -1,4 +1,9 @@
 package com.project.textprocessingtool;
 
 public class TextProcessor {
+    public static String processText(String text, String pattern, String replacement) {
+        String matches = RegexProcessor.findMatches(text, pattern);
+        String replaced = RegexProcessor.replaceText(text, pattern, replacement);
+        return "Matches:\n" + matches + "\nReplaced Text:\n" + replaced;
+    }
 }
